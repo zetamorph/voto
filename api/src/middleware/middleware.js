82746810b2b1ec9ab0ef43006447353170f5ddba
Johannes Loewe <johannes@loewe.pm>
2017-06-12 23:46:18 +0200

    added Access-Control-Allow-Methods header to all responses

--- api/src/middleware/middleware.js
+++ api/src/middleware/middleware.js
@@ -25,6 +25,8 @@ module.exports = (db) => {
     setHeaders: (req,res,next) => {
       res.header("Access-Control-Allow-Origin", "*");
       res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Auth");
+      res.header("Access-Control-Allow-Methods", "GET, POST, DELETE");
+      res.header("Access-Control-Expose-Headers", "Auth");
       next();
     }
   };
